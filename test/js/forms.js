08d06eba199c33e898ee344ef9db77bcf7e72b7c
Craig Patik <cpatik@gmail.com>
2013-05-04 17:53:00 -0400

    Indentation

--- test/js/forms.js
+++ test/js/forms.js
@@ -1,24 +1,23 @@
 $(document).ready(function () {
-
-  // Check form field validity and mark errors
-  // Based on browser validation
-  $('input, select, textarea').on('blur', function(evt) {
-    var $container = $(this).closest('.columns'),
+    // Check form field validity and mark errors
+    // Based on browser validation
+    $('input, select, textarea').on('blur', function(evt) {
+        var $container = $(this).closest('.columns'),
         classNames = 'error show';
 
-    if (this.validity) {
-      if (this.validity.valid === true) {
-        $container.removeClass(classNames);
-      }
-      else {
-        $container.addClass(classNames);
-      }
-    }
-    else if (this.willValidate === true) {
-      $container.removeClass(classNames);
-    }
-    else if (this.willValidate === false) {
-      $container.addClass(classNames);
-    }
-  });
+        if (this.validity) {
+            if (this.validity.valid === true) {
+               $container.removeClass(classNames);
+            }
+            else {
+                $container.addClass(classNames);
+            }
+        }
+        else if (this.willValidate === true) {
+            $container.removeClass(classNames);
+        }
+        else if (this.willValidate === false) {
+            $container.addClass(classNames);
+        }
+    });
 });
