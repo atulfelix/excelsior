c77e02eb254cd910e1689fb01bc9162ecd0cd389
Jeff Herb <herb.jeff@gmail.com>
2013-05-08 10:39:38 -0400

    Setting everything to run with just click

--- excelsior/js/core/excelsior.js
+++ excelsior/js/core/excelsior.js
@@ -232,7 +232,7 @@ EWF.fixImagePath = function _fixImagePath() {
     // End Zepto.js
 
 
-    (function($){var x,y,t;window.addEventListener('touchstart',function(e){x=e.touches[0].clientX;y=e.touches[0].clientY;},true);window.addEventListener('touchmove',function(e){x=e.touches[0].clientX;y=e.touches[0].clientY;},true);window.addEventListener('touchend',function(e){t=new Date();},true);$(window).on('tap doubleTap',function(e){if(!e.defaultPrevented){t=0;}});window.addEventListener('click',function(e){var time_threshold=1000,space_threshold=30;if(new Date()-t<=time_threshold&&Math.abs(e.clientX-x)<=space_threshold&&Math.abs(e.clientY-y)<=space_threshold){e.stopPropagation();e.preventDefault();}},true);}(Zepto));
+    //(function($){var x,y,t;window.addEventListener('touchstart',function(e){x=e.touches[0].clientX;y=e.touches[0].clientY;},true);window.addEventListener('touchmove',function(e){x=e.touches[0].clientX;y=e.touches[0].clientY;},true);window.addEventListener('touchend',function(e){t=new Date();},true);$(window).on('tap doubleTap',function(e){if(!e.defaultPrevented){t=0;}});window.addEventListener('click',function(e){var time_threshold=1000,space_threshold=30;if(new Date()-t<=time_threshold&&Math.abs(e.clientX-x)<=space_threshold&&Math.abs(e.clientY-y)<=space_threshold){e.stopPropagation();e.preventDefault();}},true);}(Zepto));
 
 
   } // end if(Modernizr.touch)
@@ -296,6 +296,8 @@ $(document).ready(function(){
   if (!EWF.$body) {
     EWF.$body = $('body');
   }
+
+  EWF.activateEventName = "click";
 
   // Standard Gov Banner display code
   $('#gov-link-3').on(EWF.activateEventName, function(e) {
